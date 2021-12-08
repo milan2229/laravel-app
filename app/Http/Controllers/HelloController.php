8913904f8edff45bbacb7c3b359907fc30a9d96e
milan2229 <milan_1899.22@icloud.com>
2021-12-09 00:43:25 +0900

    Initial Commit

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class HelloController extends Controller
+{
+    public function index($id='zero') {
+        $data = ['msg'=>'これはコントローラーから渡されたメッセです。',
+                    'id'=>$id
+                ];
+        return view('hello.index',$data);
+    }
+}
